58bd854915d31f7d6ac4089fb7bf952feecbef1e
Jacob Hotz <77470805+Jacob1010-h@users.noreply.github.com>
2023-11-17 19:09:23 -0800

    add scoreboard embed

--- findJerome.py
+++ findJerome.py
@@ -2,6 +2,37 @@
 import discord
 from discord.ext import commands
 
+class ScoreboardEmbed(discord.Embed):
+    def __init__(self, bot, found):
+        super().__init__(
+            title="Scoreboard",
+            url = None,
+            description="Who is winning?! It's every person for themselves! With that being said...here are the scores:", 
+            color=0xff8000
+        )
+        
+        images = []
+        for user_id, score in found.items():
+            user = bot.get_user(user_id)
+            if user is None:
+                continue
+            self.add_field(
+                name=user.name, 
+                value=f"Score: {score['score']}",
+                inline=False
+            )
+            if score["image"] is not None:
+                images.append(score["image"][-1])
+
+        if len(images) > 0:
+            self.add_field(
+                name="Last Found",
+                value="",
+                inline=False
+            )
+            self.set_image(url=images[-1])
+
+
 class FindJerome(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -9,18 +40,15 @@ class FindJerome(commands.Cog):
 
     @commands.hybrid_command(name="score" , help="Shows the scoreboard for everyone who has found Jerome", with_app_command=True)
     async def getScoreBoard(self, ctx):
-        embed = discord.Embed(title="Jerome Scoreboard", description="The number of times each person has found Jerome", color=0x00ff00)
-        for user_id, count in self.found_count.items():
-            user = self.bot.get_user(user_id)
-            embed.add_field(name=user.name, value=count, inline=False)
+        embed = ScoreboardEmbed(self.bot, self.found_count)
         await ctx.send(embed=embed)
 
     @commands.hybrid_command(name="found", help="Found Jerome! Increases your score by 1", with_app_command=True)
-    async def found(self, ctx):
+    async def found(self, ctx, image: discord.Attachment):
         user = ctx.author
         if user.id not in self.found_count:
-            self.found_count[user.id] = 1
-        else:
-            self.found_count[user.id] += 1
+            self.found_count[user.id] = {"score": 0, "image": []}
+        self.found_count[user.id]["score"] += 1
+        self.found_count[user.id]["image"].append(image)
         
-        await ctx.send(f"{user.name} has found it {self.found_count[user.id]} times!")
+        await ctx.send(f"{user.name} has found Jerome {self.found_count[user.id]['score']} times!")
